5eefbf51818f75217e2662c2ec85f55a5f248e0c
2014bcs010 <openjamosesopm@gmail.com>
2018-05-31 16:03:49 +0300

    Updates

--- src/e/marks/TCPServer.java
+++ src/e/marks/TCPServer.java
@@ -9,54 +9,66 @@ package e.marks;
  *
  * @author john
  */
+import core.Create_Excel;
+import core.Pick_GeneralNext;
+import core.Pick_GeneralNumeric;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TCPServer extends Thread {
-
+    
     final static int TCP_SERVER_PORT = 9701;
     private Socket socket;
-    Object[] datas = null;
-    ArrayList< Object[]> allobj = new ArrayList<Object[]>();
-
+    
     public TCPServer(Socket sock) {
         // TODO Auto-generated constructor stub
         socket = sock;
-        datas = new Object[]{"Reg Number", "Marks"};
-        allobj.add(datas);
-
+        
     }
-
+    
     public void run() {
-
         String path = "/Users/john/Documents/E-Marks/";
         String file_name = "students_marks.xlsx";
         String sheet_name = "marks";
-
+        Object[] datas = null;
+        ArrayList< Object[]> allobj = new ArrayList<Object[]>();
+        datas = new Object[]{"Reg Number", "Marks"};
+        allobj.add(datas);
+        try {
+            List<String> reg_num = Pick_GeneralNext.pick(path + file_name, 0, 0, 1);
+            List<Double> marks = Pick_GeneralNumeric.pick_3(path + file_name, 0, 1, 1);
+            for (int i = 0; i < reg_num.size(); i++) {
+                datas = new Object[]{reg_num.get(i), marks.get(i)};
+                allobj.add(datas);
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.err.println(" ********* File not found..! *********** ");
+        }
+        
         System.out.println(this.socket.getPort() + " working or sleeping for 5 seconds");
-
+        
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
+        
         DataInputStream clientinp;
         DataOutputStream clientout;
-
+        
         try {
             clientinp = new DataInputStream(socket.getInputStream());
             clientout = new DataOutputStream(socket.getOutputStream());
             System.out.println("here");
-
             StringBuffer inputLine = new StringBuffer();
             while (true) {
-
                 //if () {
                 try {
                     String tmp;
@@ -71,6 +83,7 @@ public class TCPServer extends Thread {
                                 allobj.add(datas);
                             }
                         }
+                        
                         Create_Excel.createExcel2(allobj, path + file_name, sheet_name);
                     }
                     /**
@@ -88,16 +101,15 @@ public class TCPServer extends Thread {
                 String sentence = clientinp.readUTF();
                 System.out.println("not here now");
                 System.out.println(sentence);
-
+                
                 clientout.writeBytes(sentence);
-
+                
             }
-
+            
         } catch (IOException e) {
-
             System.out.println(e.getStackTrace());
         } finally {
-
+            
             try {
                 this.socket.close();
             } catch (IOException e) {
@@ -106,15 +118,15 @@ public class TCPServer extends Thread {
             }
         }
     }
-
+    
     public static void main(String args[]) throws IOException {
         ServerSocket serversocket;
         serversocket = new ServerSocket(TCP_SERVER_PORT);
-
+        
         while (true) {
             Socket clientsocket = serversocket.accept();
             new TCPServer(clientsocket).start();
-
+            
         }
     }
 }
